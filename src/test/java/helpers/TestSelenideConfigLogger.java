6a2c948dbd3cff50189e10458421f669a938039c
Alex Ov <alexov@swisscows.email>
2023-04-26 14:25:37 +0300

    Add a method to cover all Selenide base parameters logging

--- src/test/java/helpers/TestSelenideConfigLogger.java
+++ src/test/java/helpers/TestSelenideConfigLogger.java
@@ -7,33 +7,42 @@ import org.slf4j.LoggerFactory;
 @SuppressWarnings("all")
 public class TestSelenideConfigLogger {
 
-    Logger logger =  LoggerFactory.getLogger(TestSelenideConfigLogger.class);
+    Logger logger = LoggerFactory.getLogger(TestSelenideConfigLogger.class);
 
-    public static TestSelenideConfigLogger getTestSelenideConfigLogger(){
+    public static TestSelenideConfigLogger getTestSelenideConfigLogger() {
         return new TestSelenideConfigLogger();
     }
 
-    public TestSelenideConfigLogger logBrowserName(){
+    public void logSelenideMainConfigValues() {
+        getTestSelenideConfigLogger()
+                .logBrowserName()
+                .logBrowserSize()
+                .logBrowserVersion()
+                .logBrowserBaseUrl()
+                .logBrowserRemote();
+    }
+
+    public TestSelenideConfigLogger logBrowserName() {
         logger.info("Selenide browser name: " + Configuration.browser);
         return this;
     }
 
-    public TestSelenideConfigLogger logBrowserSize(){
+    public TestSelenideConfigLogger logBrowserSize() {
         logger.info("Selenide browser size: " + Configuration.browserSize);
         return this;
     }
 
-    public TestSelenideConfigLogger logBrowserVersion(){
+    public TestSelenideConfigLogger logBrowserVersion() {
         logger.info("Selenide browser version: " + Configuration.browserVersion);
         return this;
     }
 
-    public TestSelenideConfigLogger logBrowserBaseUrl(){
+    public TestSelenideConfigLogger logBrowserBaseUrl() {
         logger.info("Selenide base URL: " + Configuration.baseUrl);
         return this;
     }
 
-    public TestSelenideConfigLogger logBrowserRemote(){
+    public TestSelenideConfigLogger logBrowserRemote() {
         logger.info("Selenide remote: " + Configuration.remote);
         return this;
     }
