d2c92a826d0fc9b763e9ad13a3217260b0eea589
Alex Ov <alexov@swisscows.email>
2023-04-30 17:30:53 +0300

    Add folder for json schema templates. Add custom allure listener and templates. Add GenApiSpec with general api specifications

---
+++ src/test/java/tests/api/specs/GenApiSpec.java
@@ -0,0 +1,24 @@
+package tests.api.specs;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static helpers.CustomAllureListener.withCustomTemplates;
+import static io.restassured.RestAssured.with;
+import static io.restassured.filter.log.LogDetail.BODY;
+import static io.restassured.filter.log.LogDetail.STATUS;
+import static io.restassured.http.ContentType.JSON;
+
+public class GenApiSpec {
+    public static RequestSpecification genRequestSpec = with()
+            .filter(withCustomTemplates())
+            .log().uri()
+            .log().body()
+            .contentType(JSON);
+
+    public static ResponseSpecification genResponseSpec = new ResponseSpecBuilder()
+            .log(STATUS)
+            .log(BODY)
+            .build();
+}
