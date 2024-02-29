5524957b530e41bfeb9b27a3e536615cc62a4e92
Alex Ov <alexov@swisscows.email>
2024-02-29 21:47:09 +0200

    Disable Lanit test -- may not work via VPN

---
+++ src/test/java/tests/samples/LanitTests.java
@@ -0,0 +1,23 @@
+package tests.samples;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+@Disabled("May not work via VPN")
+public class LanitTests {
+
+    @Test
+    void lanitTest01() {
+        open("https://www.lanit.ru/");
+
+        String searchText = "команда";
+
+        $(".search_header").click();
+        $(".search__input").setValue(searchText).pressEnter();
+        $(".search__item").shouldHave(Condition.text(searchText));
+    }
+}
