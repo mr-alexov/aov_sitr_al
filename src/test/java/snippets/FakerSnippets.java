5623d9b786df4a4597a4ea514dcb897fb01c6fbe
Alex Ov <alexov@swisscows.email>
2024-09-19 16:42:40 +0300

    Add some messy code for Faker tests

--- src/test/java/snippets/FakerSnippets.java
+++ src/test/java/snippets/FakerSnippets.java
@@ -4,9 +4,8 @@ import com.github.javafaker.Faker;
 
 public class FakerSnippets {
 
-    public static String getRandom() {
-        String[] numbers =
-                {"1", "2", "3"};
+    public static String getRandomNumberString() {
+        String[] numbers = {"1", "2", "3"};
         return new Faker().options().option(numbers);
     }
 
--- src/test/java/tests/libs/FakerTests.java
+++ src/test/java/tests/libs/FakerTests.java
@@ -1,16 +1,20 @@
 package tests.libs;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
-import static snippets.FakerSnippets.getRandom;
+import static snippets.FakerSnippets.getRandomNumberString;
 import static org.assertj.core.api.Assertions.*;
 
 public class FakerTests {
 
+    Faker faker = new Faker();
 
     @Disabled
     @Test
@@ -19,10 +23,24 @@ public class FakerTests {
         Set<String> results = new HashSet<>();
 
         for (int i = 0; i < 50; i++) {
-            results.add(getRandom());
+            results.add(getRandomNumberString());
         }
 
         assertThat(results).containsOnly("1","2","3");
+
+    }
+
+
+    @Test
+    void fakerDateTest() {
+        Date myDate = faker.date().birthday(17, 70);
+
+        SimpleDateFormat yearFormatter = new SimpleDateFormat("yyyy");
+
+        String year = yearFormatter.format(myDate);
+
+        System.out.println(year);
+
 
     }
 }
